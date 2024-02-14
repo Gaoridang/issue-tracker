c8fb4bf6a0af488c2b3d1c412d24344e8abc4f89
Lee JaeJun <monooi@naver.com>
2024-02-14 21:10:41 +0900

    Secure the application

--- app/api/issues/route.ts
+++ app/api/issues/route.ts
@@ -1,8 +1,13 @@
 import { NextRequest, NextResponse } from "next/server";
 import prisma from "@/prisma/client";
 import { issueSchema } from "../../validationSchemas";
+import { authOptions } from "@/app/lib/auth";
+import { getServerSession } from "next-auth";
 
 export async function POST(request: NextRequest) {
+  const session = await getServerSession(authOptions);
+  if (!session) return NextResponse.json({}, { status: 401 });
+
   const body = await request.json();
   const validation = issueSchema.safeParse(body);
   if (!validation.success) return NextResponse.json(validation.error.errors, { status: 400 });
--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -3,12 +3,15 @@ import { notFound } from "next/navigation";
 import IssueDetails from "./IssueDetails";
 import IssueEditButton from "./IssueEditButton";
 import IssueDeleteButton from "./IssueDeleteButton";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/app/lib/auth";
 
 interface Props {
   params: { id: string };
 }
 
 const IssueDetailPage = async ({ params: { id } }: Props) => {
+  const session = await getServerSession(authOptions);
   const issue = await prisma.issue.findUnique({
     where: { id: parseInt(id) },
   });
@@ -20,10 +23,12 @@ const IssueDetailPage = async ({ params: { id } }: Props) => {
       <div className='col-span-4'>
         <IssueDetails issue={issue} />
       </div>
-      <div className='col-span-4 md:col-span-1 flex flex-col gap-4'>
-        <IssueEditButton issueId={issue.id} />
-        <IssueDeleteButton issueId={issue.id} />
-      </div>
+      {session && (
+        <div className='col-span-4 md:col-span-1 flex flex-col gap-4'>
+          <IssueEditButton issueId={issue.id} />
+          <IssueDeleteButton issueId={issue.id} />
+        </div>
+      )}
     </div>
   );
 };
